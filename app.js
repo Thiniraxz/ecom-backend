c4550f88be1a70f1c2b3091d170c23488e4cfac7
Thiniraxz <thiniragenuka@gmail.com>
2024-08-16 23:07:03 +0530

    Configure appjs

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ const cors = require("cors");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const cartRouter = require("./routes/cartRoutes");
 
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(helmet());
 
 if (process.env.NODE_ENV === "development") {
@@ -25,8 +31,11 @@ app.use(cookieParser());
 app.use(mongoSanitize());
 
 //Routes
+app.use("/files", express.static("files"));
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/cart", cartRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
